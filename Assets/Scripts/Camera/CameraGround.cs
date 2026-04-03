7fbf4ffa718411fd2380cb1b088e0c01cbff929e
CroustiLoan <l.miquel-renaud@mtp.ecolescreatives.com>
2026-04-03 14:54:15 +0200

    Modif pour mockup

--- Assets/Scripts/Camera/CameraGround.cs
+++ Assets/Scripts/Camera/CameraGround.cs
@@ -13,7 +13,7 @@ public class CameraGround : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = new Vector3(player.transform.position.x + 6, player.transform.position.y + 3, -10);
+        Vector3 desiredPosition = new Vector3(player.transform.position.x + 6.5f, player.transform.position.y + 2.5f, -10);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
