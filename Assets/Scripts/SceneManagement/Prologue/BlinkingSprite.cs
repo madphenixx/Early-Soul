5a6e10b11b033dfc5d12b929d669fb0001b9a3fe
madphenixx <loane.gicquel@gmail.com>
2026-04-06 16:39:22 +0200

    Blinking light

---
+++ Assets/Scripts/SceneManagement/Prologue/BlinkingSprite.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlinkingSprite : MonoBehaviour
+{
+    private SpriteRenderer image;
+    [SerializeField]
+    private Sprite sprite1;
+    [SerializeField] 
+    private Sprite sprite2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image = gameObject.GetComponent<SpriteRenderer>();
+        StartCoroutine(Glitch2());
+    }
+
+    private IEnumerator Glitch1()
+    {
+        yield return new WaitForSeconds(0.2f);
+        image.sprite = sprite1;
+
+        StartCoroutine(Glitch2());
+
+        //backgroundImage.sprite = sprite2;
+        //yield return new WaitForSeconds(10f);
+    }
+
+    private IEnumerator Glitch2()
+    {
+        int time = Random.Range(1, 5);
+        yield return new WaitForSeconds(time);
+        image.sprite = sprite2;
+        
+        StartCoroutine(Glitch1());
+    }
+}
