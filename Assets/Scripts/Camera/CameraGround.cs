e68724594b5a09059192b72652eb2391718ceed4
Madphenixx <loane.gicquel@gmail.com>
2026-04-07 14:02:36 +0200

    Gate change

--- Assets/Scripts/Camera/CameraGround.cs
+++ Assets/Scripts/Camera/CameraGround.cs
@@ -4,7 +4,7 @@ public class CameraGround : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
-    [SerializeField] private float smoothSpeed = 0.3f;
+    [SerializeField] private float smoothSpeed = 0.125f;
 
     public bool isFollowing = true;
     public bool isZooming = false;
@@ -20,7 +20,7 @@ public class CameraGround : MonoBehaviour
         if (isFollowing == true)
         {
             Vector3 desiredPosition = new Vector3(player.transform.position.x + 6f, player.transform.position.y + 2f, -10);
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed - 1);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
 
             gameObject.GetComponent<Camera>().orthographicSize = 5;
@@ -31,7 +31,7 @@ public class CameraGround : MonoBehaviour
             isFollowing = false;
 
             Vector3 desiredPosition = new Vector3(-58.55f, 3.11f, -3.7f);
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * 0.4f);
             transform.position = smoothedPosition;
 
             gameObject.GetComponent<Camera>().orthographicSize = 2.73f;
--- Assets/Scripts/SceneManagement/Prologue/GateSpawn.cs
+++ Assets/Scripts/SceneManagement/Prologue/GateSpawn.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 public class GateSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject gate;
+    [SerializeField] private GameObject props;
 
     [SerializeField] private Vector3 gatePos;
 
     void Start()
     {
+        props = GameObject.Find("Props");
        gatePos = new Vector3(-19,-1, -1); 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && PrologueManager.dialogueLucyPlay)
         {
-            Instantiate(gate, gatePos, Quaternion.identity);
+            Instantiate(gate, gatePos, Quaternion.identity, props.transform);
             Destroy(gameObject);
         }
     }
