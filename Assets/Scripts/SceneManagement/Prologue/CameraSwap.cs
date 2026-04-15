a417e0924d240cb439a13138f583bc9430d764ff
GAME1 <GAME1@ecmtp.local>
2026-04-15 15:31:39 +0200

    Fix prologue !!!

--- Assets/Scripts/SceneManagement/Prologue/CameraSwap.cs
+++ Assets/Scripts/SceneManagement/Prologue/CameraSwap.cs
@@ -33,7 +33,7 @@ public class CameraSwap : MonoBehaviour
     //    }
     //}
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
