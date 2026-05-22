5e2e3481d140a5d04e36f6e8dc46173adda515bd
Skolastique <teillietm@gmail.com>
2026-05-22 15:30:24 +0200

    Cache pour cacher les temps de charge longs sa mère

--- Assets/Scripts/Items/Gate.cs
+++ Assets/Scripts/Items/Gate.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Gate : MonoBehaviour
 {
+    [SerializeField] private GameObject cache;
+    private void Start()
+    {
+        cache = GameObject.Find("Cache");
+    }
+
     public bool isEnabled = true;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if ((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Boat")) && isEnabled)
         {
+            cache.GetComponent<Image>().enabled = true;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             Destroy(gameObject);
         }
